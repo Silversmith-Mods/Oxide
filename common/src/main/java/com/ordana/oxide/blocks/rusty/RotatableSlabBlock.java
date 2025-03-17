87f7bc02f994bdaf97d1b4a02c7e0dc0630503f6
Ordana <62686557+AstralOrdana@users.noreply.github.com>
2025-03-17 00:29:08 -0500

    version bump

--- common/src/main/java/com/ordana/oxide/blocks/rusty/RotatableSlabBlock.java
+++ common/src/main/java/com/ordana/oxide/blocks/rusty/RotatableSlabBlock.java
@@ -2,24 +2,15 @@ package com.ordana.oxide.blocks.rusty;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.ItemInteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.SlabType;
-import net.minecraft.world.phys.BlockHitResult;
-
-import java.util.Objects;
 
 public class RotatableSlabBlock extends SlabBlock {
     public static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.AXIS;
--- common/src/main/java/com/ordana/oxide/items/CementBucketItem.java
+++ common/src/main/java/com/ordana/oxide/items/CementBucketItem.java
@@ -1,22 +1,11 @@
 package com.ordana.oxide.items;
 
-import net.minecraft.ChatFormatting;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.ScaffoldingBlock;
-import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
 
 public class CementBucketItem extends BlockItem {
     public CementBucketItem(Block block, Properties properties) {
         super(block, properties);
     }
-    
+
 }
