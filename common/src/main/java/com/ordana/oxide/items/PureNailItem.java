f2bd8e90d40bf5b390c64c209424acfe9bdebfdc
Ordana <62686557+AstralOrdana@users.noreply.github.com>
2025-03-23 23:21:57 -0500

    gradle fixes

--- common/src/main/java/com/ordana/oxide/items/PureNailItem.java
+++ common/src/main/java/com/ordana/oxide/items/PureNailItem.java
@@ -1,5 +1,6 @@
 package com.ordana.oxide.items;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
@@ -11,11 +12,15 @@ import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.MaceItem;
+import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.component.Tool;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.List;
 
@@ -39,9 +44,20 @@ public class PureNailItem extends Item {
             target.addEffect(new MobEffectInstance(MobEffects.BLINDNESS, 20, 1, true, false));
             attacker.setDeltaMovement(attacker.getLookAngle().x / 2, 0.8, attacker.getLookAngle().z / 2);
             serverPlayer.connection.send(new ClientboundSetEntityMotionPacket(serverPlayer));
-            serverLevel.playSound(null, serverPlayer.getX(), serverPlayer.getY(), serverPlayer.getZ(), SoundEvents.THORNS_HIT, serverPlayer.getSoundSource(), 5.0F, 3F / (serverLevel.random.nextFloat() * 0.4F + 0.8F));
+            serverLevel.playSound(null, serverPlayer.getX(), serverPlayer.getY(), serverPlayer.getZ(), SoundEvents.PLAYER_ATTACK_SWEEP, serverPlayer.getSoundSource(), 5.0F, 3F / (serverLevel.random.nextFloat() * 0.4F + 0.8F));
         }
 
         return true;
+    }
+
+    public boolean canAttackBlock(BlockState state, Level level, BlockPos pos, Player player) {
+        if (player instanceof ServerPlayer serverPlayer) {
+            ServerLevel serverLevel = (ServerLevel)player.level();
+
+            player.setDeltaMovement(player.isCrouching() ? -player.getLookAngle().x : player.getLookAngle().x, 0.8, player.isCrouching() ? -player.getLookAngle().z : player.getLookAngle().z);
+            serverPlayer.connection.send(new ClientboundSetEntityMotionPacket(serverPlayer));
+            level.playSound(null, serverPlayer.getX(), serverPlayer.getY(), serverPlayer.getZ(), SoundEvents.PLAYER_ATTACK_SWEEP, serverPlayer.getSoundSource(), 5.0F, 3F / (serverLevel.random.nextFloat() * 0.4F + 0.8F));
+        }
+        return !player.isCreative();
     }
 }
